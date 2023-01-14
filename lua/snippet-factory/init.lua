0b4e27f5c29a8b59bb31978590e3990a575346dd
ziontee113 <ziontee113@gmail.com>
2023-01-14 08:14:30 +0700

    Handle cursor position and V/v modes for get_text

--- lua/snippet-factory/init.lua
+++ lua/snippet-factory/init.lua
@@ -1,3 +1,7 @@
+local get_text = require "snippet-factory.lib.get_text"
+
 local M = {}
 
 return M
+
+-- {{{nvim-execute-on-save}}}
--- lua/snippet-factory/lib/get_text.lua
+++ lua/snippet-factory/lib/get_text.lua
@@ -4,14 +4,26 @@ M.get_selection_text = function()
     local start_row, start_col = vim.fn.line "v", vim.fn.col "v"
     local end_row, end_col = vim.fn.line ".", vim.fn.col "."
 
-    local text = vim.api.nvim_buf_get_text(
-        0,
-        start_row - 1,
-        start_col - 1,
-        end_row - 1,
-        end_col,
-        {}
-    )
+    if
+        start_row > end_row or (start_row == end_row and start_col >= end_col)
+    then
+        start_row, start_col = vim.fn.line ".", vim.fn.col "."
+        end_row, end_col = vim.fn.line "v", vim.fn.col "v"
+    end
+
+    local text
+    if vim.fn.mode() == "v" then
+        text = vim.api.nvim_buf_get_text(
+            0,
+            start_row - 1,
+            start_col - 1,
+            end_row - 1,
+            end_col,
+            {}
+        )
+    elseif vim.fn.mode() == "V" then
+        text = vim.api.nvim_buf_get_lines(0, start_row - 1, end_row, false)
+    end
 
     return text
 end
